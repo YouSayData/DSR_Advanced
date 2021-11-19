79163d1cfce56a59d920f78b45f29f7e0402b2a6
Thomas Koentges <thomaskoentges@brucheion.io>
2021-11-19 23:44:23 +1300

    add solution II and III

--- recap_solutions.R
+++ recap_solutions.R
@@ -28,3 +28,44 @@ corrected1 <- cleaned %>%
 corrected2 <- cleaned %>%
   mutate(reading = ifelse(quantity == "sal" & reading > 1, reading / 100, reading))
 
+
+# Exercise 2 --------------------------------------------------------------
+
+person <- read_csv("https://education.rstudio.com/blog/2020/08/more-example-exams/person.csv")
+
+summarize_table <- function(title = character(), tbl = tibble()) {
+  if (length(title) != 1 | typeof(title) != "character") {
+    stop("invalid title. Did you switch the parameters around?")
+  }
+  if (!("tbl" %in% class(tbl))) {
+    stop("you forgot the table!")
+  }
+  cat(str_c(title, " has ", dim(tbl)[1], " rows and ", dim(tbl)[2], " columns."))
+}
+
+summarize_table("our table", person)
+
+show_columns <- function(title = character(), tbl = tibble()) {
+  if (length(title) != 1 | typeof(title) != "character") {
+    stop("invalid title. Did you switch the parameters around?")
+  }
+  if (!("tbl" %in% class(tbl))) {
+    stop("you forgot the table!")
+  }
+  cat(str_c(title, " has the columns ", str_c(colnames(tbl), collapse = ", ")))
+}
+
+show_columns("our table", person)
+
+
+# Exercise 3 --------------------------------------------------------------
+
+import_data <- function(link = character()) {
+  read_csv(link,
+         na = c("-", ">95%")) %>%
+  pivot_longer(-ISO3, names_to = "type", values_to = "value") %>%
+  separate(type, into = c("year", "type"), sep = " ", convert = T) %>%
+  pivot_wider(names_from = type, values_from = value)
+}
+
+import_data("https://education.rstudio.com/blog/2020/02/instructor-certification-exams/infant_hiv.csv")
